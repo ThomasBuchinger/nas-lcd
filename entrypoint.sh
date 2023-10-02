1b0d64a98991804398c819830d835e7ede5b5094
Thomas Buchinger <ThomasBuchinger@users.noreply.github.com>
2023-10-02 18:35:15 +0200

    Fix entrypoint script, not probing all i2c busses

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ for bus_name in $(i2cdetect -l | awk '/Synopsys DesignWare I2C adapter/{ print $
   # Probe if there is a device on address 0x27 (decimal 39)
   echo "Probing: $bus_name"
   bus_number=$(echo $bus_name | cut -f 2 -d '-')
-  if $(i2cdetect -y -r 1 39 39 | grep --quiet 27 ); then 
+  if $(i2cdetect -y -r $bus_number 39 39 | grep --quiet 27 ); then 
     BUS=$bus_number
     ADDRESS="27"
     break
